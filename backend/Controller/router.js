38465cf2e34df998e7bea0273cd03d597d23c4d0
Agustin Hernandorena <agustinhernandorena@gmail.com>
2022-06-05 21:45:17 -0300

    Add create and get all user endpoints

---
+++ backend/Controller/router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("./userController");
+const userController = new UserController();
+
+router.post("/users", (req, res, nxt) => userController.create(req, res, nxt));
+
+router.get("/users", (req, res, nxt) => userController.getAll(req, res, nxt));
+
+
+// router.delete("/reserves/", (req, res, nxt) =>
+// userController.deleteReserve(req, res, nxt)
+// );
+
+// router.get("/reserves/:documentId", (req, res, nxt) =>
+// userController.getReserve(req, res, nxt)
+// );
+
+module.exports = router;
